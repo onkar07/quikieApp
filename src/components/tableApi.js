13b7ee3a6c0afb5fcf914c8f35c4b6446dba6cbd
onkar07 <34215437+onkar07@users.noreply.github.com>
2021-06-14 15:48:28 +0530

    table button

--- src/components/tableApi.js
+++ src/components/tableApi.js
@@ -1,19 +1,24 @@
+import { data } from 'jquery';
 import React, { useEffect, useLayoutEffect, useState } from 'react'
 
 function Table() {
   const [crypto, setCrypto] = useState();
   const [currentPage, setCurrentpage] = useState(1);
   const [itempp, setItemspp] = useState(5);
-  const [text, setText] = useState("save data");
-
+  const [index, setIndex] = useState();
   useEffect( () =>{
     fetch("https://api.nomics.com/v1/currencies/ticker?key=4704ee24ea5d850ad32c0644b21d284cf547af2a&interval=1d,30d&convert=EUR&per-page=100&page=1")
     .then(response => response.json())
-    .then((data)=>{setCrypto(data);console.log(crypto)});
+    .then((data)=>{
+      setCrypto(data)
+      data.forEach(element => {
+        element.saved = "false";
+      }); 
+      console.log(crypto)}
+      )
 },[])
 
     if(crypto != null){
-      console.log(crypto.length)
       const page = [];
       for(let i=0; i<Math.ceil((crypto.length)/itempp); i++){
       page.push(i);
@@ -21,38 +26,27 @@ function Table() {
     const IndexOfLastpage = currentPage + itempp;
     const IndexOfFirstpage = IndexOfLastpage - itempp;
     const CurrentItem = crypto.slice(IndexOfFirstpage,IndexOfLastpage);  
-    console.log(IndexOfLastpage,IndexOfFirstpage,CurrentItem)
-    console.log(page)
+ 
+  const checkSaved = (e) =>{
     
-    const handleClick = (e) =>{
-      e.preventDefault();
-      const Cname = e.currentTarget.id;
+    const Cname = e.currentTarget.id;
       Object.entries(crypto).map((val)=>{
           if(Cname == val[1].name){
+            console.log(val[0],index)
             console.log(val[1])
-            console.log(e.currentTarget.id) 
+            setIndex(val[0]);
+            val[1].saved = "true";
           }
       })
   }
 
-  const handlePage = (event) =>{
-    if(currentPage >= (crypto.length)){
-      alert("no page")
-    }
-    else{
-      setCurrentpage(currentPage+itempp)
-      
-    }
+  
+  const handlePage = () =>{
+    currentPage>=(crypto.length)?alert("np page"):setCurrentpage(currentPage+itempp);
     
   }
   const handlePageBackward = () =>{
-    if(currentPage==1){
-      alert("no page");
-    }
-    else{
-      setCurrentpage(currentPage-itempp)
-    }
-    
+    currentPage==1?alert("no page"):setCurrentpage(currentPage-itempp);
   }
     
 
@@ -83,13 +77,16 @@ function Table() {
                     
                       crypto?
                       Object.entries(CurrentItem).map((val,i) => {
-                        i *= 1;
+                        i += 1;
                         return(
                                 <tr key={i} id={i}>
                                   <th scope="row">{val[1].name}</th>
                                   <td><span className="symbol">•{'\u00A0'}{val[1].symbol}</span></td>
                                   <td>${val[1].market_cap%1000.00}K</td>
-                                  <td> <button className="btn btncolor" id={val[1].name} onClick={setText("yo")}>{text}</button>  </td>
+                                    {
+                                      val[1].rank - 1 == index?<td><button className="btn btncolor" id={val[1].name} onClick={checkSaved}>{"show"}</button></td>:
+                                      <td><button className="btn btncolor" id={val[1].name} onChange={()=>console.log("called")} onClick={checkSaved} >{"save data"}</button></td>
+                                    }
                                   <td>${val[1].price}<br/><sub>USD</sub></td>
                                 </tr>
                               ) 
